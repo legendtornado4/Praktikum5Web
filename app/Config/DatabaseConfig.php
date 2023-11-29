269df1bc253ca221b9c9cdade47b90cfeb822e3a
OJAN <legendtornado4@gmail.com>
2023-11-29 16:54:00 +0700

    first commit

---
+++ app/Config/DatabaseConfig.php
@@ -0,0 +1,11 @@
+<?php
+namespace app\Config;
+class DatabaseConfig
+{
+// SETTING KONFIGURASI DATABASE KALIAN
+public $host = "localhost";
+public $user = "root";
+public $password = "";
+public $database_name = "praktikum";
+public $port = 3306;
+}
